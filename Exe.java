cabacdf47dd4b506958909534d6f3040382bafec
joebassist777-ship-it <joebassist777@gmail.com>
2025-09-08 15:56:01 +0900

    Update Exe.java

--- Exe.java
+++ Exe.java
@@ -35,12 +35,16 @@ new A().a3(null);
 }
 }
 
+=====================
+package exc1;
 
+public class Nike extends Exception {
 
+}
 
 ==========================
 package exc1;
-
+//예외 정답
 public class A {
 	void a() {
 		try {
@@ -68,6 +72,71 @@ public class A {
 		}
 		b.b();
 	}
+}
+
+
+
+
+================================== 전체 예외 잡기
+package exc1;
+
+public class A {
+
+    // 1. ArrayIndexOutOfBoundsException
+    int a() {
+        try {
+            int[] arr = new int[3];
+            System.out.println(arr[-1]); // 예외 발생
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException caught");
+            e.printStackTrace();
+        } finally {
+            System.out.println("A a finally");
+            return 2;
+        }
+    }
+
+    // 2. ClassCastException
+    void a2(Object o) {
+        try {
+            B b = (B) o; // 예외 발생
+        } catch (ClassCastException e) {
+            System.out.println("ClassCastException caught");
+            e.printStackTrace();
+        }
+    }
+
+    // 3. NullPointerException
+    void a3(B b) {
+        try {
+            b.b(); // b가 null이면 예외 발생
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught");
+            e.printStackTrace();
+        }
+    }
+
+    // 4. 사용자 정의 예외 throw
+    void a4() throws Nike {
+        throw new Nike("no size");
+    }
+
+    // 5. 모든 예외를 한 번에 처리
+    void a5() {
+        try {
+            int[] arr = new int[2];
+            System.out.println(arr[5]); // ArrayIndexOutOfBoundsException
+
+            Object o = new Object();
+            B b = (B) o; // ClassCastException
+
+            B nullB = null;
+            nullB.b(); // NullPointerException
+        } catch (ArrayIndexOutOfBoundsException | ClassCastException | NullPointerException e) {
+            System.out.println("예외 발생: " + e.getClass().getSimpleName());
+            e.printStackTrace();
+        }
+    }
 }
 
 
